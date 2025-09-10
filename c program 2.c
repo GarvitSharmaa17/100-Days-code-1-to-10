d8b83995be2cb3d54a37755009a4f23733504e04
GarvitSharmaa17 <idb5340@gmail.com>
2025-09-10 18:16:59 +0530

    Update c program 2.c

--- c program 2.c
+++ c program 2.c
@@ -1,7 +1,7 @@
 
 
-/*Q1 (User Inputs, Operations & Output)
-Write a program to input two numbers and display their sum.*/
+/*Q2 (User Inputs, Operations & Output)
+Write a program to input two numbers and display their sum, difference, product, and quotient.*/
 
 
 
@@ -12,17 +12,24 @@ int main (){
 int a;
 int b;
 int sum;
+int sub;
+int product;
+float quotient;
 printf("Enter First Number : ");
 scanf("%d",&a);
 printf("Enter Second Number : ");
 scanf("%d",&b);
 
 sum=a+b;
+sub=a-b;
+product=a*b;
+quotient=a/b;
 
-printf("Sum of two number is : %d\n",sum);
+printf("Sum of two number is : %d\nDifference between two number is : %d\nProduct of two numbers is : %d\nQuotient of two numbers is : %f\n",sum,sub,product,quotient);
 
 
 return 0;
 }
+
 
 
